a331789671e76b84639fe21ef26e5f3c323cda75
hironori_takatsuka <hironori_takatsuka@typical.co.jp>
2020-07-22 10:50:02 +0900

    ARCH-19 見積でも発注でも枝番のコンボボックスが↓キーで開かない。（共通部品の作成）

---
+++ 05ソース類/Utility/WinFormControls/MitumoriNoTextBoxEx.vb
@@ -0,0 +1,50 @@
+﻿Imports System.ComponentModel
+
+Public Class MitumoriNoTextBoxEx
+
+    Protected Overrides Sub OnPaint(ByVal e As System.Windows.Forms.PaintEventArgs)
+        MyBase.OnPaint(e)
+
+        'カスタム描画コードをここに追加します。
+    End Sub
+
+    Private LinkedComboBoxValue As MitumoriNoComboBox
+
+    <Description("リンクするコンボボックスを指定します。")>
+    Public Property LinkedComboBox() As MitumoriNoComboBox
+        Get
+            Return LinkedComboBoxValue
+        End Get
+        Set(ByVal value As MitumoriNoComboBox)
+            LinkedComboBoxValue = value
+        End Set
+    End Property
+
+
+    Public Sub New()
+        MyBase.New
+
+        LinkedComboBoxValue = Nothing
+
+        ' この呼び出しは、Windows フォーム デザイナで必要です。
+        InitializeComponent()
+
+        ' InitializeComponent() 呼び出しの後で初期化を追加します。
+
+    End Sub
+
+    Private Sub TextBoxControl_PreviewKeyDown(ByVal sender As Object, ByVal e As System.Windows.Forms.PreviewKeyDownEventArgs) Handles Me.PreviewKeyDown
+        '下キーでコンボボックスを開く
+        If e.KeyCode = Keys.Down Then
+            If LinkedComboBoxValue Is Nothing Then Return
+
+            'リンクするコンボボックスを選択
+            LinkedComboBoxValue.Focus()
+            'TypComboBoxのコンボボックスをフォーカスする為に下キーを発生させる
+            SendKeys.Send("{DOWN}")
+            'Alt＋下キーでコンボボックスを開く
+            SendKeys.Send("%{DOWN}")
+        End If
+    End Sub
+
+End Class
